1dc33b86640d59021227d1d4b6ce408fc9d6d4b2
savanipoojan78 <savanipoojan78@gmail.com>
2018-08-01 18:49:27 +0530

    Some Change in the Hotels Fragment

---
+++ Application/src/main/java/com/example/android/xyztouristattractions/HotelConstruction.java
@@ -0,0 +1,32 @@
+package com.example.android.xyztouristattractions;
+
+import android.graphics.Bitmap;
+import android.net.Uri;
+
+import com.google.android.gms.maps.model.LatLng;
+
+public class HotelConstruction {
+
+    public String name;
+    public String description;
+    public String longDescription;
+    public Uri imageUrl;
+    public Uri secondaryImageUrl;
+    public LatLng location;
+    public String city;
+
+    public Bitmap image;
+    public Bitmap secondaryImage;
+    public String distance;
+
+    public HotelConstruction(String name, String description, String longDescription, Uri imageUrl,
+                      Uri secondaryImageUrl, LatLng location, String city) {
+        this.name = name;
+        this.description = description;
+        this.longDescription = longDescription;
+        this.imageUrl = imageUrl;
+        this.secondaryImageUrl = secondaryImageUrl;
+        this.location = location;
+        this.city = city;
+    }
+}
--- Application/src/main/java/com/example/android/xyztouristattractions/ui/Hotel.java
+++ Application/src/main/java/com/example/android/xyztouristattractions/ui/Hotel.java
@@ -1,20 +1,48 @@
 package com.example.android.xyztouristattractions.ui;
 
 
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.location.Location;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.content.LocalBroadcastManager;
+import android.support.v7.widget.RecyclerView;
+import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
+import android.widget.ImageView;
+import android.widget.TextView;
+import static com.example.android.xyztouristattractions.provider.HotelAttractions.HotelATTRACTIONS;
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.engine.DiskCacheStrategy;
+import com.example.android.xyztouristattractions.Constants;
+import com.example.android.xyztouristattractions.HotelConstruction;
 import com.example.android.xyztouristattractions.R;
+import com.example.android.xyztouristattractions.Utils;
+import com.example.android.xyztouristattractions.provider.HotelAttractions;
+import com.example.android.xyztouristattractions.service.UtilityService;
+import com.google.android.gms.location.FusedLocationProviderApi;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.maps.android.SphericalUtil;
+
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+import static com.example.android.xyztouristattractions.provider.TouristAttractions.ATTRACTIONS;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class Hotel extends Fragment {
 
-
+    private AttractionAdapter mAdapter;
+    private LatLng mLatestLocation;
+    private int mImageSize;
+    private boolean mItemClicked;
     public Hotel() {
         // Required empty public constructor
     }
@@ -24,7 +52,164 @@ public class Hotel extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_hotel, container, false);
+        // Load a larger size image to make the activity transition to the detail screen smooth
+        mImageSize = getResources().getDimensionPixelSize(R.dimen.image_size)
+                * Constants.IMAGE_ANIM_MULTIPLIER;
+
+        mLatestLocation = Utils.getLocation(getActivity());
+        List<HotelConstruction> attractions = loadAttractionsFromLocation(mLatestLocation);
+        mAdapter = new Hotel.AttractionAdapter(getActivity(), attractions);
+
+        View view = inflater.inflate(R.layout.fragment_main, container, false);
+        AttractionsRecyclerView recyclerView =
+                (AttractionsRecyclerView) view.findViewById(android.R.id.list);
+        recyclerView.setEmptyView(view.findViewById(android.R.id.empty));
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setAdapter(mAdapter);
+
+        return view;
+        
+    }
+    @Override
+    public void onResume() {
+        super.onResume();
+        mItemClicked = false;
+        LocalBroadcastManager.getInstance(getActivity()).registerReceiver(
+                mBroadcastReceiver, UtilityService.getLocationUpdatedIntentFilter());
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        LocalBroadcastManager.getInstance(getActivity()).unregisterReceiver(mBroadcastReceiver);
+    }
+
+    private BroadcastReceiver mBroadcastReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            Location location =
+                    intent.getParcelableExtra(FusedLocationProviderApi.KEY_LOCATION_CHANGED);
+            if (location != null) {
+                mLatestLocation = new LatLng(location.getLatitude(), location.getLongitude());
+                mAdapter.mAttractionList = loadAttractionsFromLocation(mLatestLocation);
+                mAdapter.notifyDataSetChanged();
+            }
+        }
+    };
+
+    private static List<HotelConstruction> loadAttractionsFromLocation(final LatLng curLatLng) {
+        String closestCity = HotelAttractions.getClosestCity(curLatLng);
+        if (closestCity != null) {
+            List<HotelConstruction> attractions = HotelATTRACTIONS.get(closestCity);
+            if (curLatLng != null) {
+                Collections.sort(attractions,
+                        new Comparator<HotelConstruction>() {
+                            @Override
+                            public int compare(HotelConstruction lhs, HotelConstruction rhs) {
+                                double lhsDistance = SphericalUtil.computeDistanceBetween(
+                                        lhs.location, curLatLng);
+                                double rhsDistance = SphericalUtil.computeDistanceBetween(
+                                        rhs.location, curLatLng);
+                                return (int) (lhsDistance - rhsDistance);
+                            }
+                        }
+                );
+            }
+            return attractions;
+        }
+        return null;
+    }
+
+    private class AttractionAdapter extends RecyclerView.Adapter<Hotel.ViewHolder>
+            implements Hotel.ItemClickListener {
+
+        public List<HotelConstruction> mAttractionList;
+        private Context mContext;
+
+        public AttractionAdapter(Context context, List<HotelConstruction> attractions) {
+            super();
+            mContext = context;
+            mAttractionList = attractions;
+        }
+
+        @Override
+        public Hotel.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            LayoutInflater inflater = LayoutInflater.from(mContext);
+            View view = inflater.inflate(R.layout.list_row, parent, false);
+            return new Hotel.ViewHolder(view, this);
+        }
+
+        @Override
+        public void onBindViewHolder(Hotel.ViewHolder holder, int position) {
+            HotelConstruction attraction = mAttractionList.get(position);
+
+            holder.mTitleTextView.setText(attraction.name);
+            holder.mDescriptionTextView.setText(attraction.description);
+            Glide.with(mContext)
+                    .load(attraction.imageUrl)
+                    .diskCacheStrategy(DiskCacheStrategy.SOURCE)
+                    .placeholder(R.drawable.empty_photo)
+                    .override(mImageSize, mImageSize)
+                    .into(holder.mImageView);
+
+            String distance =
+                    Utils.formatDistanceBetween(mLatestLocation, attraction.location);
+            if (TextUtils.isEmpty(distance)) {
+                holder.mOverlayTextView.setVisibility(View.GONE);
+            } else {
+                holder.mOverlayTextView.setVisibility(View.VISIBLE);
+                holder.mOverlayTextView.setText(distance);
+            }
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return position;
+        }
+
+        @Override
+        public int getItemCount() {
+            return mAttractionList == null ? 0 : mAttractionList.size();
+        }
+
+        @Override
+        public void onItemClick(View view, int position) {
+            if (!mItemClicked) {
+                mItemClicked = true;
+                View heroView = view.findViewById(android.R.id.icon);
+                DetailActivity.launch(
+                        getActivity(), mAdapter.mAttractionList.get(position).name, heroView);
+            }
+        }
+    }
+
+    private static class ViewHolder extends RecyclerView.ViewHolder
+            implements View.OnClickListener {
+
+        TextView mTitleTextView;
+        TextView mDescriptionTextView;
+        TextView mOverlayTextView;
+        ImageView mImageView;
+        Hotel.ItemClickListener mItemClickListener;
+
+        public ViewHolder(View view, Hotel.ItemClickListener itemClickListener) {
+            super(view);
+            mTitleTextView = (TextView) view.findViewById(android.R.id.text1);
+            mDescriptionTextView = (TextView) view.findViewById(android.R.id.text2);
+            mOverlayTextView = (TextView) view.findViewById(R.id.overlaytext);
+            mImageView = (ImageView) view.findViewById(android.R.id.icon);
+            mItemClickListener = itemClickListener;
+            view.setOnClickListener(this);
+        }
+
+        @Override
+        public void onClick(View v) {
+            mItemClickListener.onItemClick(v, getAdapterPosition());
+        }
+    }
+
+    interface ItemClickListener {
+        void onItemClick(View view, int position);
     }
 
 }
